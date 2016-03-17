854a15ad0640bc3fe47bc52a263f047bad777e7a
伟周 <zhouwei@igola.com>
2016-03-17 18:55:41 +0800

    217 done

---
+++ 217.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/contains-duplicate/
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        return len(nums) != len(set(nums))
