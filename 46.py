9ff89e6a96237246902ed521def683ec11299185
伟周 <zhouwei@igola.com>
2016-03-29 15:57:44 +0800

    46 done

---
+++ 46.py
@@ -0,0 +1,9 @@
+# https://leetcode.com/problems/permutations/
+import itertools
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        return list(itertools.permutations(nums))
