8067e5b7d19e3a9e2ff9954f25911eece771a5ba
zhou wei <zhouwei@igola.com>
2018-12-24 14:33:24 +0800

    709

---
+++ 709.py
@@ -0,0 +1,8 @@
+# https://leetcode.com/problems/to-lower-case/submissions/
+class Solution(object):
+    def toLowerCase(self, str):
+        """
+        :type str: str
+        :rtype: str
+        """
+        return str.lower()
