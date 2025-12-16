5aa57cefe4bea4469ca7565d74d47c5a2c59d854
zhou wei <lilaboc.cn@gmail.com>
2025-12-16 18:58:14 +0800

    3110

---
+++ 3110.py
@@ -0,0 +1,13 @@
+# https://leetcode.com/problems/score-of-a-string/
+
+class Solution:
+    def scoreOfString(self, s: str) -> int:
+        score = 0
+        for i in range(len(s) - 1):
+            score += abs(ord(s[i]) - ord(s[i + 1]))
+        return score
+
+
+print(Solution().scoreOfString("hello"))
+print(Solution().scoreOfString("zaz"))
+
