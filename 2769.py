90756dfbe85c1f97a4db795bf562140c9e388ff8
zhou wei <lilaboc.cn@gmail.com>
2025-12-16 19:11:07 +0800

    2769

---
+++ 2769.py
@@ -0,0 +1,10 @@
+# https://leetcode.com/problems/find-the-maximum-achievable-number/description/
+
+class Solution:
+    def theMaximumAchievableX(self, num: int, t: int) -> int:
+        return num + t * 2
+
+
+print(Solution().theMaximumAchievableX(4, 1))
+print(Solution().theMaximumAchievableX(3, 2))
+
